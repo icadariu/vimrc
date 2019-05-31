7eb6de8818e65a36832110eb11a4b0bca67e865c
Ionut Cadariu <ionut.cadariu@gmail.com>
2019-05-31 12:59:07 +0200

    updated vim config and change to vunldes instead of bundles

--- vim-auto_install.sh
+++ vim-auto_install.sh
@@ -8,6 +8,7 @@
 [ -f ~/.vimrc ] && mv ~/.vimrc ~/.vimrc.old
 
 # check if git is installed
+# &> /dev/null -> This is just an abbreviation for >/dev/null 2>&1. It redirects file descriptor 2 (STDERR) and descriptor 1 (STDOUT) to /dev/null.
 
 which git &> /dev/null
 [ $? -eq 1 ] && echo "git is not installed..." && exit 1
@@ -15,5 +16,8 @@ which vim &> /dev/null
 [ $? -eq 1 ] && echo "Install vim first" && exit 1
 
 git clone https://github.com/icadariu/vimrc.git ~/.vim
-
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 [ ! -h ~/.vimrc ] && ln -s ~/.vim/.vimrc ~/.vimrc
+
+# configuring all plugins
+vim +PluginInstall +qall
